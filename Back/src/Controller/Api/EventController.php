f5874fe703b624ea382d9faa197bfd911b969e27
Franck-Valet <franck.valet.pro@gmail.com>
2019-03-20 16:52:18 +0100

    Crud entity create, form, controller and twig create for the backend

---
+++ Back/src/Controller/Api/EventController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/event")
+ */
+class EventController extends AbstractController
+{
+
+}
---
+++ Back/src/Controller/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/user")
+ */
+class UserController extends AbstractController
+{
+
+}

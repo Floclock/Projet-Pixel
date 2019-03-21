6975491f9f8d31c1616bd21426758189008467c8
Franck-Valet <franck.valet.pro@gmail.com>
2019-03-21 13:51:28 +0100

    Test json converter on comment

--- Back/src/Controller/Api/CommentController.php
+++ Back/src/Controller/Api/CommentController.php
@@ -11,9 +11,32 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/comment")
+    * @Route("/api", name="api_")
  */
 class CommentController extends AbstractController
 {
+    /**
+     * @Route("/comments", name="all_comments")
+     */
+    public function findAll(CommentRepository $repo)
+    {
+        $comments = $repo->findAll();
 
+
+        foreach ($comments as $index => $currentValue) {
+         $array[$index] = [
+             'id' => $currentValue->getId(),
+             'content' => $currentValue->getContent(),
+             'createdAt' => $currentValue->getCreatedAt(),
+             'user' => $currentValue->getUser(),
+             'event' => $currentValue->getEvent()
+         ];
+         }
+
+    $jsonCategories = \json_encode($array);
+    $response = new Response($jsonCategories);
+    $response->headers->set('Content-Type', 'application/json');
+    // $response->headers->set('Access-Control-Allow-Origin', '');
+    return $response;
+    }
 }

15de4f807761c46f28e24915e7d78718d728ecdf
Franck Valet <franck.valet.pro@gmail.com>
2019-04-08 09:14:12 +0200

    modif prof

--- Back/src/Security/LoginFormAuthenticator.php
+++ Back/src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('http://92.243.25.254/%27'));
+        return new RedirectResponse($this->urlGenerator->generate('http://92.243.25.254/'));
     }
 
     protected function getLoginUrl()
